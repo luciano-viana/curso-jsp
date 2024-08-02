aaf2f962bde7695bb98520bbee90fdbf64d7b94c
luciano-viana <lucianoviana12@gmail.com>
2024-08-02 06:19:22 -0300

    Finalizado método de consulta do usuário por login

--- curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
+++ curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import connection.SingleConnectionBanco;
 import model.ModelLogin;
@@ -16,9 +17,7 @@ public class DAOUsuarioRepository {
 	}
 	
 	
-	public void gravarUsuario(ModelLogin objeto) throws Exception {
-		
-	
+	public ModelLogin gravarUsuario(ModelLogin objeto) throws Exception {
 		
 		String sql = "INSERT INTO model_login(login, senha, nome, email) VALUES (?, ?, ?, ?);";
 		PreparedStatement preparedSql = connection.prepareStatement(sql);
@@ -30,14 +29,35 @@ public class DAOUsuarioRepository {
 		
 		preparedSql.execute();
 		connection.commit();
+		
+		return this.consultaUsuario(objeto.getLogin());
 	}
 	
+	//Método para consultar usuário
 	public ModelLogin consultaUsuario(String login) throws Exception {
-		String sql = "select * from model_login where upper(login) = upper('?')";
-		PreparedStatement statement = connection.prepareStatement(sql);
-		statement.setString(1, login);
-		return null;
 		
+		//Objeto
+		ModelLogin modelLogin = new ModelLogin();
+		
+		//Preparado o SQL para consulta no BD
+		String sql = "select * from model_login where upper(login) = upper('"+login+"')";
+		
+		//Setado os parâmetros
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		//Executado o sql
+		ResultSet resultado =  statement.executeQuery();
+		 
+		while (resultado.next()) {//Se tem resultado
+			 modelLogin.setId(resultado.getLong("id"));
+			 modelLogin.setNome(resultado.getString("nome"));
+			 modelLogin.setEmail(resultado.getString("email"));
+			 modelLogin.setLogin(resultado.getString("login"));
+			 modelLogin.setSenha(resultado.getString("senha"));
+		 }
+		 
+		 
+		return modelLogin;
 		
 		
 	}
