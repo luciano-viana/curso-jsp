0a493f67639a84352d1746eccf6ad7a1b69fcffa
luciano-viana <lucianoviana12@gmail.com>
2024-09-18 07:45:48 -0300

    Passado o JSON para montagem do Gráfico

--- curso-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -25,6 +25,7 @@ import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import beandto.BeanDtoGraficoSalarioUser;
 import dao.DAOUsuarioRepository;
 
 @MultipartConfig //Anotação utilizada para preparar para upload
@@ -206,6 +207,29 @@ public class ServletUsuarioController extends ServletGenericUtil{
 				
 				response.setHeader("Content-Disposition", "attachment;filename=arquivo." + extensao);
 				response.getOutputStream().write(relatorio);
+			}
+			else if(acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("graficoSalario")) {
+				
+				String dataInicial = request.getParameter("dataInicial");
+				String dataFinal = request.getParameter("dataFinal");
+				
+				if(dataInicial == null || dataInicial.isEmpty() && dataFinal == null || dataFinal.isEmpty()) {
+					
+					BeanDtoGraficoSalarioUser beanDtoGraficoSalarioUser = daoUsuarioRepository.
+							montarGraficoMediaSalario(super.getUserLogado(request));
+					
+					ObjectMapper mapper = new ObjectMapper();
+					
+					String json = mapper.writeValueAsString(beanDtoGraficoSalarioUser);
+					
+					response.getWriter().write(json);
+					
+				}else {
+					
+					
+					
+				}
+				
 			}
 			
 			else {
